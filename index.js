0f36fe6b5441ab34bc10556df88a57d4fc528dd3
newbienewbie <webbot@sina.com>
2016-12-23 21:56:13 +0800

    添加mock数据

--- index.js
+++ index.js
@@ -2,4 +2,15 @@ const React=require('react');
 const ReactDOM=require('react-dom');
 const App=require('./lib/app');
 
+
+(function(){
+    // mock comments list
+    const comments=[
+        {name:'name1',email:'email1@email',text:'hello,world',timestamp:new Date()},
+        {name:'name2',email:'email2@email',text:'fuck,world',timestamp:new Date()},
+    ] ;
+    localStorage.setItem('localhost/ss',JSON.stringify(comments))
+
+})();
+
 ReactDOM.render( <App/>, document.getElementById("luantan") );
