12c01cf7bb9d51b2d35f541cf23706165d2eedac
newbienewbie <webbot@sina.com>
2016-12-23 21:09:39 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const React=require('react');
+const ReactDOM=require('react-dom');
+const App=require('./lib/app');
+
+ReactDOM.render( <App/>, document.getElementById("") );
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path=require('path');
+
+const PATHS={
+    lib:path.join(__dirname,'lib'),
+    dist:path.join(__dirname,'dist'),
+};
+module.exports={
+    entry:{
+        luantan:path.join(__dirname,'index.js'),
+    },
+    output:{
+        path:PATHS.dist,
+        filename:'[name].js',
+    },
+    module:{
+        loaders:[
+            {
+                test:/\.jsx?/,
+                loader:["babel-loader"],//自右向左依次加载
+                query:{
+                    "presets": ['es2015','react'],
+                },
+                include:PATHS.lib,
+            },
+            {
+                test:/\.css$/,
+                loaders: ["style-loader","css-loader" ]
+            },
+        ]
+    }
+};
